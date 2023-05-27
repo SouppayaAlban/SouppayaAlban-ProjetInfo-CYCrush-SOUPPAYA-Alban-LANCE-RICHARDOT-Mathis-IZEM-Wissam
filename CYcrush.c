ec381abdda8b2a62e6b6402a1c732a56f33debc8
SouppayaAlban <134438672+SouppayaAlban@users.noreply.github.com>
2023-05-28 00:17:27 +0200

    Update CYcrush.c

--- CYcrush.c
+++ CYcrush.c
@@ -1,23 +1,23 @@
 #include "fonctions.h"
 
 int CYcrush(int** plat, Dimensions dim){ //Fonction principale de jeu
-  //Initialisation de toutes les variables 
+  	//Initialisation de toutes les variables 
 	int var = 0;
-  int *score;
-  int points = 0;
-  score = &points;
-  int tour = 0;
-  char pseudo[100];
-  int taille=0, i=0;
+	int *score;
+  	int points = 0;
+  	score = &points;
+  	int tour = 0;
+  	char pseudo[100];
+  	int taille=0, i=0;
 	int men;
 	int check;
-  Deplacements echange;
-  char choix;
-  //Affichage du menu de jeu
-  clear();
+  	Deplacements echange;
+  	char choix;
+  	//Affichage du menu de jeu
+  	clear();
 	menu();
 	printf("                          1.Jouer\n                          2.Règles\n                          3.Options\n                          4.Quitter\n");
-  couleur(0)
+  	couleur(0)
 	do{
 		check = scanf("%d", &men);
 		buffer();
@@ -25,7 +25,7 @@ int CYcrush(int** plat, Dimensions dim){ //Fonction principale de jeu
 	printf("\n");
 	switch(men){
 		case 1:
-    	clear();
+    			clear();
 			menu();
 			printf("                     1.Nouvelle partie\n                    2.Charger Sauvegarde\n");
 			do{
@@ -33,22 +33,22 @@ int CYcrush(int** plat, Dimensions dim){ //Fonction principale de jeu
 				buffer();
 			}while(men < 1 || men > 2 || check !=1);
 			if(men==1){
-        clear();
-        menu();
-			  printf("                     Pseudo:\n");
-          //Demande du Pseudo joueur
-        	do{
-				    check = scanf("%s", pseudo);
-				    buffer();
-            while (pseudo[i] != '\0') {
-              taille+=1;
-              i++;
-            }
-			    }while(check !=1);
-        tour+=1;
+        			clear();
+        			menu();
+				printf("                     Pseudo:\n");
+         		 	//Demande du Pseudo joueur
+        			do{
+				    	check = scanf("%s", pseudo);
+				    	buffer();
+            				while (pseudo[i] != '\0') {
+              				taille+=1;
+              				i++;
+            				}
+			    	}while(check !=1);
+        			tour+=1;
 				clear();
 				plat = construct_plat(dim, score);
-        *score = 0;
+        			*score = 0;
 				affichage(plat, dim, tour, points);
 			}
 			else{
@@ -56,35 +56,35 @@ int CYcrush(int** plat, Dimensions dim){ //Fonction principale de jeu
 				charger_sauvegarde(plat,dim, &points,&tour);
 				affichage(plat,dim, tour, points);				
 			}
-		  do{
-        //Choix de mouvement du joueur
-        do{
-          echange = choixCase(plat, dim);
-          if(verifcassage(plat, dim) == 0){
-            echangeCase(plat,echange.a, echange.b, echange.c, echange.d);
-            couleur(31)
-            printf("Mouvement Incorect, réessayez\n");
-            couleur(0)
-          }
-          clear();
-          affichage(plat,dim, tour, points);				
-          sleep(1);
-          clear();
-          affichage(plat,dim, tour, points);
-        }while(verifcassage(plat, dim) == 0);
-        //Cassage de 3 (ou plus) symboles avec gravité
-		    do{
-          clear();
-				  cassage(plat,dim,score);
-          affichage(plat,dim, tour, points);
-          sleep(1);
-          clear();
-				  gravite(plat,dim);
-          affichage(plat,dim, tour, points);
-			  }while(verifcassage(plat, dim) == 1 ); //Réaction en chaine si 3 (ou plus) symboles sont alignés
-        tour +=1;
-        printf("Sauvegarder ?(O/N)\n");
-        do{
+		  	do{
+        			//Choix de mouvement du joueur
+        			do{
+          				echange = choixCase(plat, dim);
+          				if(verifcassage(plat, dim) == 0){
+            					echangeCase(plat,echange.a, echange.b, echange.c, echange.d);
+            					couleur(31)
+            					printf("Mouvement Incorect, réessayez\n");
+            					couleur(0)
+          				}
+          				clear();
+          				affichage(plat,dim, tour, points);				
+          				sleep(1);
+          				clear();
+          				affichage(plat,dim, tour, points);
+        			}while(verifcassage(plat, dim) == 0);
+        			//Cassage de 3 (ou plus) symboles avec gravité
+		    		do{
+          				clear();
+				  	cassage(plat,dim,score);
+          				affichage(plat,dim, tour, points);
+          				sleep(1);
+          				clear();
+				  	gravite(plat,dim);
+          				affichage(plat,dim, tour, points);
+			  	}while(verifcassage(plat, dim) == 1 ); //Réaction en chaine si 3 (ou plus) symboles sont alignés
+        			tour +=1;
+        			printf("Sauvegarder ?(O/N)\n");
+        			do{
 					check = scanf("%c", &choix);
 					buffer(); 
 				}while(choix !=79 && choix !=111 && choix !=110 && choix !=78 || check != 1);
@@ -97,22 +97,22 @@ int CYcrush(int** plat, Dimensions dim){ //Fonction principale de jeu
 				clear();
 				affichage(plat,dim, tour, points);
 				}
-		  }while(testVictoire(plat, dim) == 1); //Vérification de tout le plateau de jeu si des cassages sont possibles
-      compteur_symbole(plat, dim, points, pseudo+taille, &tour);
-      mise_a_jour_score(&points);
-      CYcrush(plat, dim);
+		  	}while(testVictoire(plat, dim) == 1); //Vérification de tout le plateau de jeu si des cassages sont possibles
+      			compteur_symbole(plat, dim, points, pseudo+taille, &tour);
+      			mise_a_jour_score(&points);
+      			CYcrush(plat, dim);
 			break;
 		case 3 :
-      //Affichage du menu d'options
+      			//Affichage du menu d'options
 			do{
-        clear();
+        			clear();
 				dim = option(dim);
 			}while(dim.a !=4);
 			CYcrush(plat, dim);
 			break;
 		case 2:
-      //Affichage des règles
-      clear();
+      			//Affichage des règles
+      			clear();
 			menu();
 			regle();
 			do{
@@ -125,7 +125,7 @@ int CYcrush(int** plat, Dimensions dim){ //Fonction principale de jeu
 			CYcrush(plat, dim);
 			break;
 		case 4:
-      //Quitte le jeu
+      			//Quitte le jeu
 			return 0;
 			break;
 	}
